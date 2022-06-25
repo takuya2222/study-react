83956912d8443ff825a95e9d65cef04f6b8a0e1f
takuya <hukujuth@gmail.com>
2022-06-25 10:37:02 +0900

    ♻️ export defaultの削除

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,10 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { Footer  } from '../components/Footer'
-import Links from '../components/Links'
-import Headline from '../components/Headline'
+import {Links} from '../components/Links'
+import {Headline} from '../components/Headline'
 
+// Next.jsのpages配下のものはexport defaultにする必要がある
 export default function Home() {
   return (
     <div className={styles.container}>
